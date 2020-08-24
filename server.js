97d5177d1089311e66cf9fb59b207f0776c13e17
Victor <victorreze12@gmail.com>
2020-08-24 10:57:00 -0300

    Server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const express = required ('express');
+const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
-
 app.listen(process.env.PORT || 8080);
 
-app.get('/*', function (req, res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+//PATH LOCATION STARTEGY
+
+app.get('/*', function(req,res){
+  const fullPath = path.join(__dirname + '/src/index.html');
+  console.log(" Fetching from.." + fullPath);
+    res.sendFile(fullPath);
 })
 
-console.log('Console listening!');
-
+console.log('Server started running..');
